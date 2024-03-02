c481f43eab8f7646f795b78061c9c8b83a694268
darsius <p_darius211@yahoo.com>
2024-03-02 22:12:46 +0200

    finished designing search view

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -129,4 +129,29 @@ class APICaller {
         task.resume()
     }
     
+    func getDiscoverMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/discover/movie?api_key=\(Constants.API_KEY)&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc") else {
+            print("invalid url for the top rated movies")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {data, _, error in
+            guard let data = data, error == nil else {
+                print("couldn't fetch the data for the top rated movies")
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(APIError.failedToGetData))
+            }
+        }
+        task.resume()
+    }
+    
 }
+
+
+// https://api.themoviedb.org/3/discover/movie?api_key=%3C%3Capi_key%3E%3E&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc
